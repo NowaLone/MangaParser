dcfb2359a15323454087b4c4c17e6e0e029d3dc6
Никита <fihs@outlook.com>
2020-07-26 03:44:51 +0200

    feat: added help attributes

---
+++ test/MangaParser.Parsers.Tests/Attributes/TestClassCategoryAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MangaParser.Parsers.Tests.Attributes
+{
+    /// <summary>
+    /// Used to specify the categories of a unit test class.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class TestClassCategoryAttribute : TestCategoryBaseAttribute
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestClassCategoryAttribute"/>class and applies the categories to the test class.
+        /// </summary>
+        /// <param name="testCategories">The test categories.</param>
+        public TestClassCategoryAttribute(params string[] testCategories)
+        {
+            TestCategories = testCategories.ToList();
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public override IList<string> TestCategories { get; }
+
+        #endregion Properties
+    }
+}
---
+++ test/MangaParser.Parsers.Tests/Attributes/TestMethodCategoryAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MangaParser.Parsers.Tests.Attributes
+{
+    /// <summary>
+    /// Used to specify the categories of a unit test method.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class TestMethodCategoryAttribute : TestCategoryBaseAttribute
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestMethodCategoryAttribute"/>class and applies the categories to the test method.
+        /// </summary>
+        /// <param name="testCategories">The test categories.</param>
+        public TestMethodCategoryAttribute(params string[] testCategories)
+        {
+            TestCategories = testCategories.ToList();
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public override IList<string> TestCategories { get; }
+
+        #endregion Properties
+    }
+}
